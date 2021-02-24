9e9e18e0a3f5dac0d4aaa5547e34b1a36efab2e2
Alexander <arlenross02@gmail.com>
2021-02-24 16:50:58 -0500

    More TypeDoc
    About halfway through DisplayObject.d.ts, completed Style.d.ts, Scale.d.ts.

--- typescript/graphics/DisplayObject.d.ts
+++ typescript/graphics/DisplayObject.d.ts
@@ -1,59 +1,278 @@
 import { LineStyle, FillStyle, ColorRGB, TextStyle } from "./Style";
 import { ScaleBound } from "./Scale";
 
+/**
+ * A type that represents graphic objects. Used in `map`'s return object's `graphics.items` array property.
+ * ```javascript
+ *  ...
+ * //we can tell map to render graphics:
+ *  map(d) {
+ *      return {
+ *          graphics: {
+ *              items: [
+ *                  //Our DisplayObjects go here.
+ *              ]
+ *          }
+ *      }
+ *  }
+ * ...
+ * ```
+ */
 export type DisplayObject = LineSegments | Shapes | ContourShapes | Instancing | Text | Container | Dots;
 
+/** This is a `DisplayObject`. It represents a group of one or more Line objects.  */
 export interface LineSegments {
     readonly tag: 'LineSegments';
+    /** The graphic element's render key, like an HTML `id` attribute. */
     readonly key: string;
+    /** An array of `Line` objects to be rendered. */
     readonly lines: readonly Line[];
+    /** `LineStyle` object representing the rendered group's styles. */
     readonly lineStyle: LineStyle;
 }
 
+/** 
+ * Represents a single line from point `a` to `b`. For use in the `LineSegments` `DisplayObject`. May 
+ * be declared as having optional infinite start or end values such that the line will not terminate 
+ * on one or both ends of the X axis. 
+ */
 export interface Line {
     readonly tag: 'Line';
+    /** origin point of the line */
     readonly a: Point;
+    /** end point of the line. */
     readonly b: Point;
+    /** Optional. Declares that this line is non-terminal in the negative X-axis space. */
     readonly infiniteStart?: boolean;
+    /** Optional. Declares that this line is non-terminal in the positive X-axis space. */
     readonly infiniteEnd?: boolean;
 }
 
+/**
+ * This is a `DisplayObject`. Represents a group of one or more solid primitive `Shape` objects.
+ */
 export interface Shapes {
     readonly tag: 'Shapes';
+    /** The graphic element's render key, like an HTML `id` attribute. */
     readonly key: string;
+    /** An array of `Shape` objects associated with this `Shapes` group. */
     readonly primitives: readonly Shape[];
+    /** `FillStyle` object to define the color that this group of shapes will be rendered. */
     readonly fillStyle: FillStyle;
 }
 
+/**
+ * This is a `DisplayObject`. Represents a group of one or more outlined primitive `Shape` objects.
+ */
 export interface ContourShapes {
     readonly tag: 'ContourShapes';
+    /** The graphic element's render key, like an HTML `id` attribute. */
     readonly key: string;
+    /** An array of `Shape` primitives associated with this `ContourShapes` group. */
     readonly primitives: readonly Shape[];
+    /** The `LineStyle` object representing the width and color of the rendered object's outlines.  */
     readonly lineStyle: LineStyle;
 }
 
+/**
+ * A type that represents any of the primitive `Shape` objects. For use in `Shapes` or `ContourShapes`
+ * `DisplayObject`s rendered `shapes` array.
+ * 
+ * ```javascript
+ *  ...
+ *  {
+ *      graphics: {
+ *          items: [
+ *              {
+ *                  tag: "Shapes",
+ *                  key: "myShapes",
+ *                  primitives: [
+ *                      //Shapes go here.
+ *                  ]
+ *                  fillStyle: {
+ *                      color: { r: 1, b: 0, c: 0 }
+ *                  }
+ *              }
+ *          ]
+ *      }
+ *  }
+ *  ...
+ * ```
+ */
 export type Shape = Rectangle | Circle | RoundedRectangle | Polygon | Ellipse;
 
+/**
+ * Shape Object. Defines an instance of a Rectangle shape for use in the Shapes or ContourShapes `primitives` value.
+ * Usage:
+ * ``` javascript
+ * // Inside a calculator's `map` function, this particular definition will draw 
+ * // red 24x24px squares centered at { x: index, y: value } of each rendered bar
+ * ...
+ *  {
+ *      graphics: {
+ *          items: [ 
+ *              {
+ *                  tag: "Shapes",
+ *                  key: 'rects',
+ *                  //the rectangle is a primitve
+ *                  primitives: [ 
+ *                      {
+ *                          tag: 'Rectangle',
+ *                          position: {
+ *                              x: du(d.index()),
+ *                              y: du(d.value()),
+ *                          }
+ *                          size: {
+ *                              height: px(24),
+ *                              width: px(24)
+ *                          }
+ *                      }
+ *                  ],
+ *                  fillStyle: {
+ *                      color: { r: 1, g: 0, b: 0 }
+ *                  }
+ *              }
+ *          ]
+ *      }
+ *  }
+ * ...
+ *      
+ * ```
+ */
 export interface Rectangle {
     readonly tag: 'Rectangle';
+    /** */
     readonly position: Point;
     readonly size: Size;
     readonly infiniteStart?: boolean;
     readonly infiniteEnd?: boolean;
 }
 
+/**
+ * Shape Object. Defines an instance of an `Ellipse` shape for use in the `Shapes` or `ContourShapes` `primitives` value.
+ * Usage:
+ * ``` javascript
+ * //within a map function's return object. This will draw a red ellipse centered at each rendered 
+ * //bar's value and index.
+ * ...
+ *  {
+ *      graphics: {
+ *          items: [ 
+ *              {
+ *                  tag: "Shapes",
+ *                  key: 'ellipses',
+ *                  //the ellipse is a primitve
+ *                  primitives: [ 
+ *                      {
+ *                          tag: 'Ellipse',
+ *                          position: {
+ *                              x: du(d.index()),
+ *                              y: du(d.value()),
+ *                          }
+ *                          size: {
+ *                              height: px(18),
+ *                              width: px(24)
+ *                          }
+ *                      }
+ *                  ],
+ *                  fillStyle: {
+ *                      color: { r: 1, g: 0, b: 0 }
+ *                  }
+ *              }
+ *          ]
+ *      }
+ *  }
+ * ...
+ *      
+ * ```
+ */
 export interface Ellipse {
     readonly tag: 'Ellipse';
+    /** origin point of the ellipse */
     readonly position: Point;
+    /** size of the ellipse */
     readonly size: Size;
 }
 
+/**
+ * `Shape` Object. Defines an instance of a `Circle` shape for use in the `Shapes` or `ContourShapes` `primitives` value.
+ * Usage:
+ * ``` javascript
+ * //within a map function's return object. This will draw a red circle centered at each rendered 
+ * //bar's value and index.
+ * ...
+ *  {
+ *      graphics: {
+ *          items: [ 
+ *              {
+ *                  tag: "Shapes",
+ *                  key: 'circles',
+ *                  //the circle is a primitve
+ *                  primitives: [ 
+ *                      {
+ *                          tag: 'Circle',
+ *                          center: {
+ *                              x: d.index(),
+ *                              y: d.value(),
+ *                          }
+ *                          radius: 10
+ *                      }
+ *                  ],
+ *                  fillStyle: {
+ *                      color: { r: 1, g: 0, b: 0 }
+ *                  }
+ *              }
+ *          ]
+ *      }
+ *  }
+ * ...
+ *      
+ * ```
+ */
 export interface Circle {
     readonly tag: 'Circle';
     readonly center: Point;
     readonly radius: number;
 }
 
+/**
+ * `Shape` Object. Defines an instance of a `RoundedRectangle` shape for use in the `Shapes` or `ContourShapes` `primitives` value.
+ * Usage:
+ * ``` javascript
+ * //within a map function's return object. This will draw a red rounded rect centered at each rendered 
+ * //bar's value and index.
+ * ...
+ *  {
+ *      graphics: {
+ *          items: [ 
+ *              {
+ *                  tag: "Shapes",
+ *                  key: 'roundRects',
+ *                  //the rounded rect is a primitve
+ *                  primitives: [ 
+ *                      {
+ *                          tag: 'RoundedRectangle',
+ *                          position: {
+ *                              x: du(d.index()),
+ *                              y: du(d.value()),
+ *                          }
+ *                          size: {
+ *                              height: px(16),
+ *                              width: px(24)
+ *                          }
+ *                      }
+ *                  ],
+ *                  fillStyle: {
+ *                      color: { r: 1, g: 0, b: 0 }
+ *                  }
+ *              }
+ *          ]
+ *      }
+ *  }
+ * ...
+ *      
+ * ```
+ */
 export interface RoundedRectangle {
     readonly tag: 'RoundedRectangle';
     readonly position: Point;
@@ -61,6 +280,53 @@ export interface RoundedRectangle {
     readonly radius: number;
 }
 
+/**
+ * `Shape` Object. Defines an instance of a `Polygon` shape for use in the `Shapes` or `ContourShapes` `primitives` value.
+ * Usage:
+ * ``` javascript
+ * //within a map function's return object. This will draw a red triangle rect at each rendered 
+ * //bar's value and index.
+ * ...
+ *  {
+ *      graphics: {
+ *          items: [ 
+ *              {
+ *                  tag: "Shapes",
+ *                  key: 'tris',
+ *                  //the polygon is a primitve
+ *                  primitives: [ 
+ *                      {
+ *                          tag: 'Polygon',
+ *                          points: [
+ *                              //top
+ *                              {
+ *                                  x: du(d.index()),
+ *                                  y: op(du(d.value()), '-', px(4))
+ *                              },
+ *                              //right
+ *                              {
+ *                                  x: op(du(d.index()), '+', px(4)),
+ *                                  y: du(d.value())
+ *                              },
+ *                              //left
+ *                              {
+ *                                  x: op(du(d.index()), '-', px(4)),
+ *                                  y: du(d.value())
+ *                              }
+ *                          ]
+ *                      }
+ *                  ],
+ *                  fillStyle: {
+ *                      color: { r: 1, g: 0, b: 0 }
+ *                  }
+ *              }
+ *          ]
+ *      }
+ *  }
+ * ...
+ *      
+ * ```
+ */
 export interface Polygon {
     readonly tag: 'Polygon';
     readonly points: readonly Point[];
--- typescript/graphics/Style.d.ts
+++ typescript/graphics/Style.d.ts
@@ -1,10 +1,20 @@
+/**
+ * Represents the style used to render a line.
+ */
 export interface LineStyle {
+    /** width in pixels of the rendered line */
     readonly lineWidth: number;
+    /** color of the rendered line */
     readonly color: Color;
+    /** Optional. Opacity of the rendered line. */
     readonly opacity?: number;
+    /** Optional. Defaults to `DashLineStyle.Solid`. See `DashLineStyle` for accepted values. */
     readonly lineStyle?: DashLineStyle | string;
 }
 
+/**
+ * An enum that holds accepted values for the `lineStyle` property of the `LineStyle` interface.
+ */
 export enum DashLineStyle {
     Solid = 1,
     ThreeOne = 2,
@@ -13,26 +23,58 @@ export enum DashLineStyle {
     FourFour = 5
 }
 
+/** 
+ * Defines a `Shapes` object's fill-style. 
+ */
 export interface FillStyle {
+    /** 
+     * the fill color. Can be a Web Color string in either named (`'red'`)
+     */
     readonly color: Color;
+    /** number value between 0 and 1, where 1 represents 100% opacity. */
     readonly opacity?: number;
 }
 
+/**
+ * Defines a 'Text' graphic object's `style` property.
+ * ```javascript
+ *  //18pt bold face font
+ *  const myFontStyle = {
+ *      fontSize: 18,
+ *      fontWeight: 'bold'
+ *  }
+ * ```
+ */
 export interface FontStyle {
+    /** Optional. A string representing the font family to use. Ex: `'monospace, Courier New'` */
     readonly fontFamily?: string;
+    /** a number representing the font point size. */
     readonly fontSize: number;
+    /** Optional. The style to apply to the font, such as 'italic'. */
     readonly fontStyle?: string;
+    /** Optional. Weight of the font. Will accept a string such as "bold". */
     readonly fontWeight?: string;
+    /** Optional. Represents space between letters in pixels. */
     readonly letterSpacing?: number;
+    /** Optional. Text line height. Represents the vertical space in pixels that this line of text takes up. */
     readonly lineHeight?: number;
 }
 
+/**
+ * Definition for Text fill style.
+ */
 export interface TextStyle extends FontStyle {
     readonly fill?: Color;
 }
 
+/**
+ * A hexadecimal or named web color string
+ */
 export type Color = number | string;
 
+/**
+ * An object that represents RGB color.
+ */
 export interface ColorRGB {
     readonly r: number;
     readonly g: number;
