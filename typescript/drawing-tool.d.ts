e9774d4d1777a771626a57311b3bc5c2c050c3f0
Victor Vins <vvins@tradovate.com>
2021-08-29 10:01:12 -0400

    draft for drawing tools

---
+++ typescript/drawing-tool.d.ts
@@ -0,0 +1,76 @@
+import { Point } from "./graphics/DisplayObject";
+import { GraphicsResponse } from "./graphics/GraphicsResponse"
+import { LineStyle } from "./graphics/Style";
+
+interface DrawingTool {
+  /** Unique identifier associated with the indicator. */
+  readonly name: string;
+  /** Calculation algorithms */
+  readonly drawing: DrawingToolImplementation;
+  /** Readable text that is used in the UI. Optional. */
+  readonly description?: string;
+  /** Set of input parameters. Optional. */
+  readonly params?: ParameterDefinitions;
+  /** Output results for displaying. Optional. If not specified, the application will expect a numeric output or `value` field of the output object. */
+  readonly plots?: Plots;
+  /** How to group with other similar indicators in UI dropdown menu. */
+  readonly tags?: readonly string[];
+}
+
+interface DrawingArgs {
+  readonly props: { [parameterName: string]: number | boolean };
+  readonly plots: { [parameterName: string]: LineStyle };
+  readonly anchors: readonly Point[];
+  readonly state: any;
+}
+
+interface DrawingToolImplementation {
+  render(args: DrawingArgs): GraphicsResponse;
+  update?: (args: DrawingArgs) => { newState: any };
+  anchorRestraints?: (args: DrawingArgs) => readonly AnchorRestraint[];
+  anchorStyles?: (args: DrawingArgs) => readonly AnchorStyle[];
+  tooltips?: (args: DrawingArgs) => readonly DrawingTooltip[];
+}
+
+type CoordinateRestraint = number | [number, number];
+
+interface AnchorRestraint {
+    readonly x?: CoordinateRestraint;
+    readonly y?: CoordinateRestraint;
+}
+
+interface AnchorStyle {
+    readonly color: Color;
+}
+
+export interface DrawingTooltip {
+    readonly coord: Point;
+    readonly items: readonly DrawingTooltip.Item[];
+    readonly alignment?: DrawingTooltip.Alignment;
+}
+
+export module DrawingTooltip {
+    export interface Item {
+        readonly content: string | number | Date | { delta: number } | ScaleBound;
+        readonly key?: string;
+        readonly title?: string;
+    }
+
+    export interface PredefinedAlignment {
+        readonly tag: 'predef',
+        readonly x: 'left' | 'center' | 'right';
+        readonly y: 'above' | 'center' | 'below';
+    }
+
+    export interface OrthogonalAlignment {
+        readonly tag: 'orthogonal',
+        readonly diagonalPoint: Point;
+    }
+
+    export interface ContinuationAlignment {
+        readonly tag: 'continuation',
+        readonly diagonalPoint: Point;
+    }
+
+    export type Alignment = PredefinedAlignment | OrthogonalAlignment | ContinuationAlignment;
+}
