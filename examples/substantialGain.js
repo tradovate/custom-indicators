bee373cce6c69287bf146e1fa3d89dc43a15e6b3
Alexander <arlenross02@gmail.com>
2021-11-19 09:42:56 -0500

    Update substantialGain.js

--- examples/substantialGain.js
+++ examples/substantialGain.js
@@ -1,133 +1,236 @@
-const { op, px, du } = require('./tools/graphics')
-const predef = require('./tools/predef')
+const predef = require("./tools/predef")
+const { px, du, op } = require("./tools/graphics")
 
-function MyTracker(magicNumber) {
-    function tracker(value) {
-        return tracker.push(value)
-    }
-    
-    tracker.push = (value) => {
-        let number = value - tracker.state.last
-        let result = number >= magicNumber
-        tracker.state.last = value
-        return [result, number]
-    }
-    
-    tracker.reset = () => {
-        tracker.state = {
-            last: -1
+const Trender = {
+
+    render({anchors, props}) {
+        const upperDiff = anchors[2]
+            ? anchors[2].y.value - anchors[1].y.value
+            : 0
+            
+        if(anchors[2]) {
+            anchors[2].x.value = anchors[0].x.value
         }
-    }
-    
-    tracker.reset()
-    
-    return tracker
-}
-
-class SubstantialGain {
-    init() {
-        this.tracker = MyTracker(this.props.magicNumber)
-    }
-
-    map(d) {
-        const [shouldDraw, difference] = this.tracker(d.value())
-        
         return {
-            graphics: shouldDraw && {
+            items: [
+                {
+                    tag: 'LineSegments',
+                    key: 'lower',
+                    lines: [
+                        {
+                            tag: 'Line',
+                            a: anchors[0],
+                            b: anchors[1]
+                        }
+                    ],
+                    lineStyle: {
+                        width: 2,
+                        color: props.lower
+                    }
+                },
+                {
+                    tag: 'LineSegments',
+                    key: 'upper',
+                    lines: [
+                        {
+                            tag: 'Line',
+                            a: anchors[2] 
+                                ? {
+                                    x: anchors[0].x,
+                                    y: du(anchors[0].y.value + upperDiff)
+                                }
+                                : anchors[0],
+                            b: anchors[2] 
+                                ? {
+                                    x: anchors[1].x,
+                                    y: anchors[2].y
+                                }
+                                : anchors[0]
+                        }
+                    ],
+                    lineStyle: {
+                        width: 2,
+                        color: props.upper
+                    }
+                },
+                {
+                    tag: 'LineSegments',
+                    key: 'middle',
+                    lines: [
+                        {
+                            tag: 'Line',                            
+                            a: anchors[2] 
+                                ? {
+                                    x: anchors[0].x,
+                                    y: du(anchors[0].y.value + upperDiff/2) 
+                                }
+                                : anchors[0],
+                            b: anchors[2] 
+                                ? {
+                                    x: anchors[1].x,
+                                    y: du(anchors[1].y.value + upperDiff/2)
+                                }
+                                : anchors[0]
+                        }
+                    ],
+                    lineStyle: {
+                        width: 2,
+                        color: props.middle,
+                        lineStyle: 3
+                    },
+                }
+            ]
+        }
+    },
+
+    tooltips({anchors, props}) {
+        const deltaARaw = anchors[1] ? anchors[0].y.value - anchors[1].y.value : 0,
+              deltaBRaw = anchors[2] ? anchors[2].y.value - anchors[1].y.value : 0,
+              deltaA    = Math.abs(deltaARaw),
+              deltaB    = Math.abs(deltaBRaw)
+              
+        return [
+            {
+                coord: anchors[0],
+                alignment: {
+                    tag: 'predef',
+                    x: 'center',
+                    y: 'below'
+                },
                 items: [
-                     {
-                        conditions: {
-                            scaleRangeX: { min: 30 }
-                        },
-                        tag: "Text",
-                        key: "bigText",
-                        text: `+ ${difference}`,
-                        point: {
-                            x: op(du(d.index()), '-', px(4)),
-                            y: op(du(d.value()), '-', px(84))
-                        },
-                        style: { fontSize: 20, fontWeight: "bold", fill: "#0bf" },
-                        textAlignment: "centerMiddle"
-                    },
                     {
-                        tag: 'Container',
-                        key: 'mediumContainer',
-                        conditions: {
-                            scaleRangeX: { min: 10 }
-                        },
-                        children: [
-                            {
-                                tag: "Text",
-                                key: "ex",
-                                point: {
-                                    x: op(du(d.index()), '-', px(2)),
-                                    y: op(du(d.value()), '-', px(58)),
-                                },
-                                text: "!",
-                                style: { fontSize: 18, fontWeight: "bold", fill: "#fff" },
-                                textAlignment: "centerMiddle"
-                            },
-                            {
-                                tag: 'Shapes',
-                                key: 'circles',
-                                primitives: [
-                                    {
-                                        tag: 'Circle',
-                                        radius: 10,
-                                        center: {
-                                            x: op(du(d.index()), '-', px(2)),
-                                            y: op(du(d.value()), '-', px(60))
-                                        }
-                                    }
-                                ],
-                                fillStyle: {
-                                    color: '#5b5'
-                                }
-                            }
-                        ],
-                    },
+                        key: 'a0',
+                        content: anchors[0].y.value
+                    }
+                ]
+            },
+            {
+                coord: anchors[1] || anchors[0],
+                alignment: {
+                    tag: 'predef',
+                    x: 'right',
+                    y: 'below'
+                },
+                items: [
+                    //price-point
                     {
-                        tag: 'Container',
-                        key: 'tinyContainer',
-                        conditions: {
-                            scaleRangeX: { min: 0, max: 10 },
-                        },
-                        children: [
-                            {
-                                tag: 'Dots',
-                                key: 'dots',
-                                dots: [
-                                    { 
-                                        point: {
-                                            x: du(d.index()), 
-                                            y: op(du(d.value()), '-', px(48))
-                                        }, 
-                                        color: {
-                                            r: .25, 
-                                            g: .8, 
-                                            b: .25
-                                        } 
-                                    }
-                                ],
-                                style: {
-                                    lineWidth: 6,
-                                    color: '#5c5'
-                                }
-                            },
-                        ],
+                        key: 'a1',
+                        content: anchors[1] ? anchors[1].y.value : 0
+                    },
+                    //tick delta
+                    {
+                        title: 
+                            deltaARaw < 0 ? 'min. A to B Long: '
+                        :   deltaARaw > 0 ? 'max. A to B Short: '
+                        :                   ' - ',
+                        key: 'lowerMin',
+                        content: { delta: deltaA }
                     },
                 ]
+            },
+            {
+                coord: 
+                    anchors[1] 
+                    ? {
+                        x: anchors[1].x,
+                        y: du(anchors[1].y.value + deltaB/2)
+                    }
+                    : anchors[0],
+                alignment: {
+                    tag: 'predef',
+                    x: 'right',
+                    y: 'center'
+                },
+                items: [
+                    {
+                        key: 'a2.5',
+                        content: anchors[1] ? anchors[1].y.value + deltaB/2 : 0
+                    },
+                    {
+                        title: 
+                            deltaARaw < 0 ? 'avg. A to B Long: ~'
+                        :   deltaARaw > 0 ? 'avg. A to B Short: ~'
+                        :                   ' - ',
+                        key: 'mid',
+                        content: 
+                            deltaARaw > 0 ? { delta: deltaA - deltaB/2 }
+                        :   deltaARaw < 0 ? { delta: deltaA + deltaB/2 }
+                        :                   { delta: 0 }
+                    },
+                ]
+            },
+            {
+                coord: 
+                    anchors[1] 
+                    ? {
+                        x: du(anchors[1].x.value),
+                        y: du(anchors[1].y.value + deltaB)
+                    }
+                    : anchors[0],
+                alignment: {
+                    tag: 'predef',
+                    x: 'right',
+                    y: 'above'
+                },
+                items: [
+                    //price
+                    {
+                        key: 'a1.5',
+                        content: anchors[1] ? anchors[1].y.value + deltaB : 0
+                    },
+                    //delta
+                    {
+                        title: 
+                            deltaARaw < 0 ? 'max. A to B Long: '
+                        :   deltaARaw > 0 ? 'min. A to B Short: '
+                        :                   ' - ',
+                        key: 'lowerMax',
+                        content: 
+                            deltaARaw > 0 ? { delta: deltaA - deltaB }
+                        :   deltaARaw < 0 ? { delta: deltaA + deltaB }
+                        :                   { delta: 0 }
+                    },
+                ]
+            },
+            //top right
+            {
+                coord: anchors[2] ? { x: anchors[0].x, y: du(anchors[2].y.value - deltaA) } : anchors[0],
+                alignment: {
+                    tag: 'predef',
+                    x: 'center',
+                    y: 'above'
+                },
+                items: [
+                    {
+                        key: 'a2',
+                        content: anchors[2] ? anchors[2].y.value - deltaA : 0
+                    }
+                ]
             }
-        }
+        ]
+    },
+    anchorRestraints({anchors}) {
+        const anch1 =  anchors[2] ? 0 : 1000 
+        return [ {}, {x: anch1},{x: [anchors[1].x.value, anchors[1].x.value] } ]    
+    },
+
+    anchorStyles({props}) {
+        return [
+            { color: props.lower },
+            { color: props.lower },
+            { color: props.upper }
+        ]
     }
 }
 
 module.exports = {
-    name: "substantialGain",
-    description: "SubstantialGain",
-    calculator: SubstantialGain,
+    name: "Price Channel",
+    description: "Price Channel",
+    drawing: Trender,
     params: {
-        magicNumber: predef.paramSpecs.number(1.00, 0.25, 0.25)
+        upper: predef.paramSpecs.color('#2d2'),
+        middle: predef.paramSpecs.color('#999'),
+        lower: predef.paramSpecs.color('#d22')
     },
-    tags: ["My Tools"],
+    maxN: 3
 }
