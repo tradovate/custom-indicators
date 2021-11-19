40b2980a611715f5412c30160301e9ce61d2e955
Alexander <arlenross02@gmail.com>
2021-11-19 09:53:05 -0500

    Update priceChannel.js

--- examples/priceChannel.js
+++ examples/priceChannel.js
@@ -85,9 +85,13 @@ const Trender = {
 
     tooltips({anchors, props}) {
         const deltaARaw = anchors[1] ? anchors[0].y.value - anchors[1].y.value : 0,
-              deltaBRaw = anchors[2] ? anchors[2].y.value - anchors[0].y.value : 0,
+              deltaBRaw = anchors[2] ? anchors[2].y.value - anchors[1].y.value : 0,
               deltaA    = Math.abs(deltaARaw),
-              deltaB    = Math.abs(deltaBRaw)
+              deltaB    = Math.abs(deltaBRaw),
+              topLeft   = 
+                anchors[2] && anchors[1].y.value < anchors[0].y.value ? anchors[2].y.value + deltaA
+              : anchors[2] && anchors[1].y.value > anchors[0].y.value ? anchors[2].y.value - deltaA
+              :                                                         0
               
         return [
             {
@@ -192,9 +196,9 @@ const Trender = {
                     },
                 ]
             },
-            //top right
+            //top left
             {
-                coord: anchors[2] ? { x: anchors[0].x, y: anchors[2].y } : anchors[0],
+                coord: anchors[2] ? { x: anchors[0].x, y: du(topLeft) } : anchors[0],
                 alignment: {
                     tag: 'predef',
                     x: 'center',
@@ -203,14 +207,15 @@ const Trender = {
                 items: [
                     {
                         key: 'a2',
-                        content: anchors[2] ? anchors[2].y.value : 0
+                        content: topLeft
                     }
                 ]
             }
         ]
     },
     anchorRestraints({anchors}) {
-        return [ {}, {x: 0}, {x: [anchors[1].x.value, anchors[1].x.value] } ]    
+        const anch1 =  anchors[2] ? 0 : 1000 
+        return [ {}, {x: anch1},{x: [anchors[1].x.value, anchors[1].x.value] } ]    
     },
 
     anchorStyles({props}) {
